73c3cccd95aee29eac16200bcb3f620d26d3e678
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 17:54:12 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 1 Exercício 4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem =  "\n"+ " Não adianta olhar para a tecnologia sem olhar para as pessoas"+
+                               "\n"+ " Mulher, só você é capaz de descobrir a sua força "+
+                               "\n"+ " Mergulhe nesse universo, o futuro é feminino!";
+
+            Console.WriteLine($"Mensagem de Incentivo: {mensagem}");
+        }
+    }
+}
